879d5f887702bc1c37eb9fe22f8929ab34822342
mac <hafiz.siddiq.umer@gmail.com>
2023-09-09 12:01:29 +0500

    API view completed

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import GetIngredientApiView
+from .views import GetIngredientApiView, DeleteIngredientApiView
 
 urlpatterns = [
     path(
         'api/ingredients/',
         GetIngredientApiView.as_view(),
         name='ingredient-list',
+    ),
+    path(
+        'api/ingredients/<int:ingredient_id>/',
+        DeleteIngredientApiView.as_view(),
+        name='ingredient-delete',
     ),
 ]
